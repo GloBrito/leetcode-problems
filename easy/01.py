8efa814000f39bce7b3db2628c499fcc194495cc
Glória Brito <gloriafbrito@gmail.com>
2023-01-13 00:58:19 -0300

    start the first easy exercise python

---
+++ easy/01.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+    
+    
+vogais = "a", "e", "i", "o", "u"
+
+
+
+
+
+# https://leetcode.com/problems/reverse-vowels-of-a-string/
