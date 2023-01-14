b3001451cff39ad22eaed1032a1fcb9eef38a797
Glória Brito <gloriafbrito@gmail.com>
2023-01-14 01:54:23 -0300

    adiciona breve explicação

--- easy/02.py
+++ easy/02.py
@@ -4,14 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """ 
-        nums = [1,1,1,3,3,4,3,2,4,2]
         confere_numero = set()
 
-        for cada_numero in nums:
-            if cada_numero in confere_numero:
-                return True
+        for cada_numero in nums: #para cada número da lista:
+            if cada_numero in confere_numero: #se conter números repetidos:
+                return True #retorne verdadeiro
 
-            confere_numero.add(cada_numero)
-        return False
+            confere_numero.add(cada_numero) #método que adiciona o elemento repetido ao conjunto.
+        return False #Se conter números distintos, retorna falso. 
 
-        print(cada_numero)
+
+# link
+# https://leetcode.com/problems/contains-duplicate/
+
+#  Os sets(conjuntos) em python são utilizados ​​para armazenar vários itens em uma única variável e que gera itens não duplicados, não indexados e  imutáveis (não podem ser alterados, ms podem ser removidos ou adicionado novos itens).
+
+# Referências
+# https://realpython.com/python-sets/
